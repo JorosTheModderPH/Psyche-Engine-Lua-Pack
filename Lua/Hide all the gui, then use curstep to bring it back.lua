18c9c6d7524f8563457f14de0ab66164a2afb9d5
JorosTheCharter <83757828+JorosTheModderPH@users.noreply.github.com>
2022-02-20 02:22:01 +0800

    Add files via upload

---
+++ Lua/Hide all the gui, then use curstep to bring it back.lua
@@ -0,0 +1,16 @@
+function onCreate()
+	setProperty('camHUD.alpha', 0);
+	setProperty('camGame.alpha', 0);
+end
+
+function onSongStart()
+    setProperty('camGame.alpha', 1);
+end
+
+function onStepHit()
+    if curStep == 100 then --change the step to whichever time of the song you want te hud to fade in
+	doTweenAlpha('hud', 'camHUD', 1, 6, "quartInOut")
+	end
+end
+
+--By AetherLynx
