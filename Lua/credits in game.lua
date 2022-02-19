c68b0567489ad80e452cb934e68d12f5a14a4265
JorosTheCharter <83757828+JorosTheModderPH@users.noreply.github.com>
2022-02-20 04:18:37 +0800

    Add files via upload

---
+++ Lua/credits in game.lua
@@ -0,0 +1,9 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Insert your watermark here", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
+
+--by someone
+--credit for showcasing the script : Uhard999
+--link video : https://youtu.be/DmXorF8PIxc
