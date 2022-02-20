5bd369a8701c1100197adcae3ce8e428c8430c86
JorosTheCharter <83757828+JorosTheModderPH@users.noreply.github.com>
2022-02-21 05:56:22 +0800

    opponentplay is different :/

--- Lua/drain_health_for_play_as_opponent.lua
+++ Lua/drain_health_for_play_as_opponent.lua
@@ -15,3 +15,4 @@ end
 --script made by me
 --Mods and Script
 --note : This script is for opponent play from the lemz1 opponent play build. If you want to use opponent play for your mod, then open this link (https://github.com/lemz1/FNF-PsychEngine/tree/opponentplay) and then you will be able to use the script. Also, the opponent's play was a bit buggy.
+--One more thing, if you're using lemz1 opponent play, I recommend you change the health gain to 2x, or it will not work. I tried everything I could to make a different health drain for BF and the opponent, but I still couldn't find a way.
