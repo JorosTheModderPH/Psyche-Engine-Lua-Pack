bf83d96e7132cd87746e4d1969a0e94f807c3034
JorosTheCharter <83757828+JorosTheModderPH@users.noreply.github.com>
2022-03-08 06:29:54 +0800

    update

--- Lua/Avoid Health Drain when singing sustained notes(Player).lua
+++ Lua/Avoid Health Drain when singing sustained notes(Player).lua
@@ -1,6 +1,10 @@
+function onUpdatePost() 
 function goodNoteHit(id, noteData, noteType, isSustainNote)
     curHealth = getProperty('health');
 	if isSustainNote then
-		    setProperty('health', curHealth - 0.023);--based on health gain, it can be bypassed when your health is x2 or more.
+		if curHealth > 0.023 then 
+		    setProperty('health', curHealth - 0.023);
+		end
+	end
 end
 end
