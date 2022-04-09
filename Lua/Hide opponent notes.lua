4500100fb98e4e20b6811817c89ef90c75b179af
JorosTheCharter <83757828+JorosTheModderPH@users.noreply.github.com>
2022-04-09 13:39:40 +0800

    Add files via upload

---
+++ Lua/Hide opponent notes.lua
@@ -0,0 +1,6 @@
+function onCreatePost()
+noteTweenAlpha('first', 0, 0, 0.01, 'linear');
+noteTweenAlpha('second', 1, 0, 0.01, 'linear');
+noteTweenAlpha('third', 2, 0, 0.01, 'linear');
+noteTweenAlpha('fourth', 3, 0, 0.01, 'linear');
+end
